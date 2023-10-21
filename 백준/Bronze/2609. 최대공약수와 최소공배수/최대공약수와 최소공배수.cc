84b5a8743abf4959ea2dbff1a1af1dcff73cd4a6
Jaeho Son <88009937+thswogh@users.noreply.github.com>
2023-10-21 14:53:53 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -1,23 +1,13 @@
 #include <stdio.h>
 #define Min(a, b) a > b ? b : a
-#define Max(a, b) a > b ? a : b
 int main()
 {
-    int a, b, gcd, lcm;
+    int a, b, gcd;
     scanf("%d %d", &a, &b);
     int min = Min(a, b);
-    int max = Max(a, b);
     for (int i = 1; i <= min; i++)
         if (a % i == 0 && b % i == 0)
             gcd = i;
-    for (int i = max; i <= a * b; i++)
-    {
-        if (i % a == 0 && i % b == 0)
-        {
-            lcm = i;
-            break;
-        }
-    }
-    printf("%d\n%d", gcd, lcm);
+    printf("%d\n%d", gcd, a * b / gcd);
 }
 // 최대공약수, 최소공배수
