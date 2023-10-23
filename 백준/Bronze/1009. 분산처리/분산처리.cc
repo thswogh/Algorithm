3d9900552d13dafcce0e0d32df8c78879fadc0ce
Jaeho Son <88009937+thswogh@users.noreply.github.com>
2023-10-24 06:49:07 +0900

    [Bronze II] Title: 분산처리, Time: 800 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/1009. 분산처리/분산처리.cc
+++ 백준/Bronze/1009. 분산처리/분산처리.cc
@@ -1,16 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    int N, a, b;
+    int N, a, b,num;
     scanf("%d", &N);
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < N; ++i)
     {
         scanf("%d %d", &a, &b);
-        int num = 1;
-        for (int j = 1; j <= b; j++)
-        {
-            num = (num * a) % 10;
-        }
+        num = 1;
+        for (int j = 1; j <= b; ++j)
+            num = num * a % 10;
         if (num == 0)
             printf("10\n");
         else
